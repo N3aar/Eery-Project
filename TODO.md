86577d95965544b87951d02a8970a30ba26633d5
Psykka <36780964+Psykka@users.noreply.github.com>
2024-05-30 01:49:12 -0300

    feat: basic commands
    Co-authored-by: Gabriel Tavares <N3aar@users.noreply.github.com>

--- TODO.md
+++ TODO.md
@@ -2,6 +2,8 @@ TODO:
 - [ ] XP System
 - [ ] Fix Links from Social Medias
 - [ ] Remind Roll Command / Notify after 15 Minutes
+- [x] Utilities Commands
+- [x] Color with Roles
 
 Anime Component:
 - [ ] Anilist Command
---
+++ src/commands/guild/gbanner.ts
@@ -0,0 +1,47 @@
+import { EmbedColors } from "@/utils/contants.js";
+import { Command } from "@sapphire/framework";
+import { EmbedBuilder, type ImageURLOptions } from "discord.js";
+
+export class GuildBannerCommand extends Command {
+	public constructor(context: Command.LoaderContext, options: Command.Options) {
+		super(context, { ...options });
+	}
+
+	public override registerApplicationCommands(registry: Command.Registry) {
+		registry.registerChatInputCommand((builder) =>
+			builder.setName("gbanner").setDescription("Mostrar o banner do Servidor"),
+		);
+	}
+
+	public async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
+		const guild = interaction.guild;
+		if (!guild) return;
+
+		const config: ImageURLOptions = {
+			extension: "png",
+			size: 4096,
+			forceStatic: false,
+		};
+
+		const guildIcon = guild?.bannerURL(config);
+
+		if (!guildIcon) {
+			return interaction.reply({
+				content: "Servidor não possui um Banner!",
+				ephemeral: false,
+				fetchReply: false,
+			});
+		}
+
+		const embed = new EmbedBuilder()
+			.setTitle(guild.name)
+			.setImage(guildIcon)
+			.setColor(EmbedColors.default);
+
+		await interaction.reply({
+			embeds: [embed],
+			ephemeral: false,
+			fetchReply: false,
+		});
+	}
+}
