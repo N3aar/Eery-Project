bfc15d52c9e6c96c02096b8dfa350c6aa11b24bb
Psykka <36780964+Psykka@users.noreply.github.com>
2024-05-29 21:34:27 -0300

    feat: basic command client
    Co-authored-by: Gabriel Tavares <N3aar@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
-const main = () => {
-	console.log("Hello World");
-	console.log(process.env.TOKEN);
-};
+import { LogLevel, SapphireClient } from "@sapphire/framework";
+import { GatewayIntentBits } from "discord.js";
 
-main();
+const client = new SapphireClient({
+	defaultPrefix: "!",
+	caseInsensitiveCommands: true,
+	logger: {
+		level: process.env.NODE_ENV ? LogLevel.Debug : LogLevel.Info,
+	},
+	intents: [
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.MessageContent,
+	],
+	loadMessageCommandListeners: true,
+});
+
+client.login(process.env.TOKEN);
